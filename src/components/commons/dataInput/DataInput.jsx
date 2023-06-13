ddc8bf31849173c2cba70a78a667d5c548660782
sj <yseojin1010@naver.com>
2023-06-13 16:59:11 +0900

    Co-authored-by: jongtae.nam <MAIN6419@users.noreply.github.com> Co-authored-by: YunjungKang <Yunjung324@users.noreply.github.com> Co-authored-by: NASTAR <an1413@users.noreply.github.com>

--- src/components/commons/dataInput/DataInput.jsx
+++ src/components/commons/dataInput/DataInput.jsx
@@ -1,33 +1,7 @@
 import styled from "styled-components";
 
-const InpLabel = styled.label`
-    font-size: 16px;
-    color: #767676;
-    font-weight: 500;
-    margin-bottom: 15px;
-    display: block;
-    margin-top: 40px;
-    @media screen and (max-width: 768px) {
-        font-size: 14px;
-    };
-`;
+import { DataInputStyle } from "./InputStyle.js";
 
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 12px;
-    @media screen and (max-width: 768px){
-        margin-top: 0px;
-    };
-`;
-
-export default function UserInput(props) {
-    return (
-        <Wrapper>
-            <InpLabel htmlFor={props.inputId}>
-                {props.label}
-            </InpLabel>
-            {props.children}
-        </Wrapper>
-    );
-};
+export default function DataInput() {
+  return <DataInputStyle></DataInputStyle>;
+}
